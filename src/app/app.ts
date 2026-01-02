61a61a840bbb3e48f573c2c15f4c93800b667a13
Breanna Chase <breannachase1@gmail.com>
2026-01-02 15:25:01 -0700

    added selection forms to the selection component and created the api service

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CarSelection } from "./car-selection/car-selection";
+import { CarView } from "./car-view/car-view";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CarSelection, CarView],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/car-selection/car-selection.ts
+++ src/app/car-selection/car-selection.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-car-selection',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './car-selection.html',
   styleUrl: './car-selection.css'
 })
-export class CarSelection {
 
+export class CarSelection {
+  selectedMake = '';
+  selectedModel = '';
+  selectedYear = '';
+
+  makes = ['Toyota', 'Honda', 'Ford'];
+  models: string[] = [];
+  years: number[] = [];
+
+  submitMake() {
+
+    if (this.selectedMake === 'Toyota') {
+      this.models = ['Camry', 'Corolla'];
+    }
+    if (this.selectedMake === 'Honda') {
+      this.models = ['Civic', 'Accord'];
+    }
+    if (this.selectedMake === 'Ford') {
+      this.models = ['F150', 'Mustang'];
+    }
+  }
+  submitModel() {
+
+    this.years = [2020, 2021, 2022];
+  }
+
+  submitYear() {
+    console.log('Final selection:', {
+      make: this.selectedMake,
+      model: this.selectedModel,
+      year: this.selectedYear
+    });
+  }
 }

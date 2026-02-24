4946352386fe7502366ae8a424e3b283db05f0d5
bchas63 <bchas@wgu.edu>
2026-02-24 11:43:18 -0700

    api string change

--- src/app/services/car-api.ts
+++ src/app/services/car-api.ts
@@ -7,7 +7,7 @@ import { Car } from '../car-api-service/car';
   providedIn: 'root',
 })
 export class CarApi {
-  private apiUrl = 'https://0j8pu88ffd.execute-api.us-east-1.amazonaws.com/prod/cars';
+  private apiUrl = 'https://l4gos4wmf4.execute-api.us-east-1.amazonaws.com/prod/cars';
 
   constructor(private http: HttpClient) {}
 
